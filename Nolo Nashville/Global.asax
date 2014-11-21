8f2c4cef8c9c7509d1789a15499c358388d137a8
Christopher Anderson <christopher.bl.anderson@gmail.com>
2014-11-21 09:54:31 -0600

    Inital Commit

---
+++ Nolo Nashville/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nolo_Nashville.MvcApplication" Language="C#" %>
---
+++ Nolo Nashville/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nolo_Nashville.Startup))]
+namespace Nolo_Nashville
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
